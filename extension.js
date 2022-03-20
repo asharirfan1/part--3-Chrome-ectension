201a3769149fcf77010e90e1ff99412738973a66
Ashar <ashrirfan26@gmail.com>
2022-03-20 11:17:58 -0700

    initial commit

---
+++ extension.js
@@ -0,0 +1,42 @@
+
+ //myLeads = [ "ahsar", "per", "john"]
+//create a empty array so we can add user data
+
+  myLeads = []
+
+const inputEl = document.getElementById("input-el")
+
+const inputBtn = document.getElementById("input-btn")
+const ulEl =  document.getElementById("ul-el")
+inputBtn.addEventListener("click", function () {
+
+    // console.log("button was clciked")
+
+// /    myLeads.push ("www.awesomeleads.com") // push what user writes to array 
+       
+        myLeads.push(inputEl.value)  // push whatever is written in input box to myLeads array
+        inputEl.value = ""  // to clear input value when clicked 
+     renderLeads()
+
+})
+function renderLeads(){
+
+  let listItems = ""
+
+  for ( i =0; i < myLeads.length ; i++) {  // checking to render out on page 
+  
+      //console.log (myLeads[i])
+    
+        // listItems += "<li> <a  target = '_blank' href ='myLeads[i]'>" + myLeads[i] + " </a></li>" // for space 
+        
+        listItems += `<li>
+                           <a  target = '_blank' href ='${myLeads[i]}'>
+                           
+                           ${myLeads[i]} </a>
+                   
+                </li>` // for space 
+   
+      }
+               ulEl.innerHTML = listItems
+  
+}
